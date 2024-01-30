4002179573c1508fb8233e10fb49b300ba6dac2a
Nosha08 <99495313+Nosha08@users.noreply.github.com>
2024-01-30 13:16:13 -0500

    a

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,3 +1,9 @@
 import './style.css'
 
 const api = 'https://opentdb.com/api.php?amount=10'
+const response = await fetch(api)
+const data = await response.json()
+
+for (let i=0; i++; i<data.length) {
+  console.log(data[i])
+}
