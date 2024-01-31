fb516b1020fc4b48ca3d94e6d0287bb0badefefa
Nosha08 <99495313+Nosha08@users.noreply.github.com>
2024-01-31 13:10:25 -0500

    a

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,9 +1,16 @@
 import './style.css'
 
+const DOMSelectors = {
+  app: document.querySelector('#app')
+}
+
 const api = 'https://opentdb.com/api.php?amount=10'
 const response = await fetch(api)
 const data = await response.json()
+console.log(data.results)
 
-for (let i=0; i++; i<data.length) {
-  console.log(data[i])
+for (let i=0; i<data.results.length;) {
+  console.log(data.results[i])
+  DOMSelectors.app.insertAdjacentHTML('beforeend', 
+  `<h1>${data.results[i].question}</h1>`)
 }
